ac9aa5f989238de9eec55a0ecd722887fb7094dc
YmniyKakYtka <jmaster21042005@mail.ru>
2022-11-12 17:54:15 +0300

    12.11.22 PZ_6_1

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -3,3 +3,32 @@
 списке четные числа в порядке возрастания их индексов, а затем — все нечетные
 числа в порядке убывания их индексов.
 '''
+
+import random
+
+iter = 0
+list = []
+
+while iter < 10:
+    list.append(random.randint(0, 100))
+    iter += 1
+
+print(f'Массив: {list}')
+
+iter = 0
+
+print('Чётные числа в порядке возрастания:')
+
+while iter < len(list):
+    if list[iter] % 2 == 0:
+        print(list[iter])
+    iter += 1
+
+iter = len(list) - 1
+
+print('Нечётные числа в порядке убывания:')
+
+while iter >= 0:
+    if list[iter] % 2 != 0:
+        print(list[iter])
+    iter -= 1
