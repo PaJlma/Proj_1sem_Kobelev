77f6fcc16dde03dd7c3cd29d4055c351dd6e441a
YmniyKakYtka <jmaster21042005@mail.ru>
2023-02-04 16:27:40 +0300

    commit 04.02.2023

---
+++ PZ_13/PZ_13_2.py
@@ -0,0 +1,28 @@
+'''
+В матрице найти среднее арифметическое положительных элементов, кратных 3.
+'''
+
+import random
+from functools import reduce
+
+matrix = [[], []]
+
+for i in range(0, random.randint(5, 25)):
+    matrix[0].append(random.randint(-25, 25))
+    matrix[1].append(random.randint(-25, 25))
+
+list = [i for i in matrix[0]] + [i for i in matrix[1]] #Представление матрицы в одномерном массиве
+filtered = [i for i in list if i % 3 == 0 and i > 0] #Отфильтровывание через списковое включение
+
+if len(filtered) != 0:
+    sum = reduce(lambda x, y: x + y, filtered) #Сумма всех элементов отфильтрованного массива
+    average = sum/len(filtered) #Вычисление среднего арифметического
+
+    print(f'Исходная матрица:\n'
+          f'{matrix[0]}\n'
+          f'{matrix[1]}\n'
+          f'Отфильтрованная матрица со значениями, кратными 3: {filtered}\n'
+          f'Среднее арифмметическое: {average}')
+
+else:
+    print('Фильтр не нашёл элементов, кратных 3')
