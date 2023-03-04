bdaee4029a47783cc649bc2263b1a79605349f76
zaitcevavic <zaitcev.igor18@gmail.com>
2023-03-04 15:32:16 +0300

    commit 04.03.2023

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,9 +1,15 @@
+'''
+В исходном текстовом файле(Dostoevsky.txt найти все варианты фамилии
+Достоевского (т.е. с различными окончаниями, например, Достоевский,
+Достоевского) в единственном экземпляре.
+'''
+
 import re
 
 with open('Dostoevsky.txt', 'r', encoding='utf-8') as file:
     data = file.read()
 
-    lastNamesPattern = re.compile(r'(?i)\bДостоевс\w*')
-    lastNames = re.findall(lastNamesPattern, data)
-    lastNames = set(lastNames)
+    lastNamesPattern = re.compile(r'(?i)\bДостоевс\w*') #Создание паттерна поиска
+    lastNames = re.findall(lastNamesPattern, data) #Поиск фамилий
+    lastNames = set(lastNames) #Удаление фамилий с однинаковыми склонениями
     print(lastNames)
