63504c541854991b439950987094821d350b88ac
YmniyKakYtka <jmaster21042005@mail.ru>
2023-03-10 14:10:37 +0300

    commit 10.03.2023

--- PZ_16/figures/triangle/code.py
+++ PZ_16/figures/triangle/code.py
@@ -7,12 +7,6 @@ defaultB = 2
 defaultC = 8
 
 def trianglePerimeter(a=defaultA, b=defaultB, c=defaultC):
-    if a == None:
-        a = defaultA
-    if b == None:
-        b = defaultB
-    if c == None:
-        c = defaultC
     return a+b+c
 
 def triangleArea(a=defaultA, b=defaultB, c=defaultC):
