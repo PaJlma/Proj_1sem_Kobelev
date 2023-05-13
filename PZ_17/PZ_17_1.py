2ceb81e2c325176315f2a03c7173900d5961391d
YmniyKakYtka <jmaster21042005@mail.ru>
2023-05-13 15:26:22 +0300

    PZ_17 done

---
+++ PZ_17/PZ_17_1.py
@@ -0,0 +1,25 @@
+class Calculator:
+    @staticmethod
+    def sum(first, second):
+        return first + second
+
+    @staticmethod
+    def substract(first, second):
+        return first - second
+
+    @staticmethod
+    def multiply(first, second):
+        return first * second
+
+    @staticmethod
+    def division(first, second):
+        if second != 0:
+            return first / second
+        else:
+            return ValueError('Нельзя делить на ноль!')
+
+print(Calculator.sum(2, 2))
+print(Calculator.substract(2, 2))
+print(Calculator.multiply(2, 2))
+print(Calculator.division(2, 2))
+print(Calculator.division(2, 0))
